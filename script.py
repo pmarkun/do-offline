b72d78642a289094dbad61c3c0c6abb0672720bc
Pedro Markun <pedro@markun.com.br>
2019-02-28 17:58:48 -0300

    refatorado

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfFileMerger
 import shutil
 import os
 import subprocess
-
+import datetime
 
 BASE_URL = "http://diariooficial.imprensaoficial.com.br/doflash/prototipo/"
 DIRECTORY = "{}/{}/{}/{}/pdf/"
@@ -11,57 +11,69 @@ PG = "pg_{}.pdf"
 
 ano = 2019
 mes = 'Fevereiro'
-dia = 27
+dia = datetime.datetime.now().day
 caderno = 'legislativo'
 
 
-def getPagina(ano,mes,dia,caderno,pg):
-    d = DIRECTORY.format(ano,mes,dia,caderno)
-    p = PG.format(str(pg).zfill(4))
+class DO:
+    def __init__(self, ano, mes, dia, caderno):
+        self.ano = ano
+        self.mes = mes
+        self.dia = dia
+        self.caderno = caderno
+        self.pg = 1
+        self.base_url = "http://diariooficial.imprensaoficial.com.br/doflash/prototipo/"
+        self.path = "{}/{}/{}/{}/pdf/".format(ano,mes,dia,caderno)
+        self.do_filepath = "DO_{}_{}_{}_{}.pdf".format(self.caderno,self.ano,self.mes,self.dia)
 
-    if not os.path.exists(d):
-        os.makedirs(d)
+    def filename(self):
+        return "pg_{}.pdf".format(str(self.pg).zfill(4))
 
-    url = BASE_URL + d + p
-    resp = urlopen(url)
-    if resp.code == 200:
-        with open(d+p, 'wb') as out_file:
-            shutil.copyfileobj(resp, out_file)
-        return 1
-    else:
-        return None
+    def getPagina(self):
+        print("Baixando pg "+str(self.pg))
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
 
-def getDO(ano,mes,dia,caderno):
-    pg = 1
-    download = 1
-    while download == 1:
-        d = DIRECTORY.format(ano,mes,dia,caderno)
-        p = PG.format(str(pg).zfill(4))
-        if not os.path.isfile(d+p):
-            do = getPagina(ano,mes,dia,caderno,pg)
-            if do:
-                pg += 1
-            else:
-                download = 0
+        url = self.base_url + self.path + self.filename()
+        print(url)
+        resp = urlopen(url)
+
+        if resp.code == 200:
+            with open(self.path + self.filename(), 'wb') as out_file:
+                shutil.copyfileobj(resp, out_file)
+            return 1
         else:
-            print("arquivo ja baixado")
-            pg += 1
+            return None
 
-def mergeDO(ano,mes,dia,caderno):
-    d = os.path.dirname(os.path.abspath(__file__)) + "/" + DIRECTORY.format(ano,mes,dia,caderno)
-    print(d)
-    x = [a for a in os.listdir(d) if a.endswith(".pdf")]
-    x.sort()
-    print(x)
+    def getDO(self):
+        download = 1
+        while download == 1:
+            if not os.path.isfile(self.path + self.filename()):
+                do = self.getPagina()
+                if do:
+                    self.pg += 1
+                else:
+                    download = 0
+            else:
+                self.pg += 1
 
-    merger = PdfFileMerger()
+    def mergeDO(self):
+        x = [a for a in os.listdir(self.path) if a.endswith(".pdf")]
+        x.sort()
+        print(x)
 
-    for pdf in x:
-        merger.append(open(d+pdf, 'rb'))
+        merger = PdfFileMerger()
 
-    with open("DO_{}_{}_{}_{}.pdf".format(caderno,ano,mes,dia), "wb") as fout:
-        merger.write(fout)
+        for pdf in x:
+            merger.append(open(self.path+pdf, 'rb'))
 
+        with open(self.do_filepath, "wb") as fout:
+            merger.write(fout)
+
+    def compactDO(self):
+        outfile = '-sOutputFile=s_'+self.do_filepath
+        gscmd = ['gs', '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4', '-dPDFSETTINGS=/screen', '-dNOPAUSE', '-dQUIET', '-dBATCH', outfile, self.do_filepath]
+        gsproc = subprocess.call(gscmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 #REFATORAR CODIGO P/ USAR CLASSES
 #RODAR gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dBATCH  -dQUIET -sOutputFile=output.pdf input.pdf
